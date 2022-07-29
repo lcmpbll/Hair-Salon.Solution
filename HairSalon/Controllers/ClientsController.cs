46794128af8090b478d7c81874a16f8ee97ef70b
Liam Cammpbell <liam22campbell@gmail.com>
2022-07-29 11:59:14 -0700

    Add routes for create and details

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -25,7 +25,7 @@ namespace HairSalon.Controllers
 
     public ActionResult Create()
     {
-      ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name", "Specialty");
+      ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
       return View();
     }
 
