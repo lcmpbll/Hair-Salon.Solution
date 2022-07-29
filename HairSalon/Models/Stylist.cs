73625adfeba7864fcfa454e4d7ef63dc0915dbd8
Liam Cammpbell <liam22campbell@gmail.com>
2022-07-29 08:34:10 -0700

    Update file and namespaces

---
+++ HairSalon/Models/Stylist.cs
@@ -0,0 +1,13 @@
+namespace HairSalon.Models
+{
+  public class Stylist
+  {
+    public Stylist()
+    {
+      this.Clients = new HashSet<Client>();
+    }
+
+    public int StylistId { get; set; }
+    public virtual ICollection<Client> Clients { get; set; }
+  }
+}
